ea91cdba0613df97d99d96e37c643565531b6867
Kyle Woolford <kyle.woolford@gmail.com>
2024-01-30 16:27:59 -0500

    File is able to search now

--- PA0_Code.py
+++ PA0_Code.py
@@ -1,14 +1,11 @@
 import pandas as pd
-# user_champ = "Blank"
-# user_ability = "Blank"
-# user_cdr = "Blank"
 
 df = pd.read_excel("Champ_Cooldowns_Fixed.xlsx")
 print(df)
 
 def userInput():
     user_champ = input("What is the name of the Champion? : ").title()
-    user_ability = input("Which ability will you max first? : ")
+    user_ability = input("Will you max Q, W, or E first? : ")
     user_cdr = input("How much ability haste do you anticipate to have? : ")
     return user_champ, user_ability, user_cdr
 
@@ -18,5 +15,7 @@ def champSearch(df, user_champ):
 
 user_data = userInput()
 print(user_data[0])
-champ_data = champSearch(df, user_data[0])
-print(champ_data)
+
+print(df.loc[df["Champion"] == user_data[0]])
+# champ_data = champSearch(df, user_data[0])
+# print(champ_data)
